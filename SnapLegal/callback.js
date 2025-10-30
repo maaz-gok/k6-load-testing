7b92fa863fcc366478fb7e48cbe27d56f7ac0450
maaz-gok <maaz@geeksofkolachi.com>
2025-10-30 18:49:31 +0500

    Edit

--- SnapLegal/callback.js
+++ SnapLegal/callback.js
@@ -4,7 +4,7 @@ import { options } from "../Scenarios.js";
 import { endpoints } from "../config/endpoints.js";
 export { options };
 const token =
-  "ya29.A0AQQ_BDRoa1u20bOxa4CZKtXe8CyKF74KnZFelZ98xsN_tNSTxQvzhKoWKekMMf_9lkWjUcoTZ93ehV0rc6iYtpjdzzuPyBoQAbireUDF1687hNM3aV1zWJuMPdxvBbVrHqdcANo3nkSffx_pYCPTOmb2zTgGKymmNCorLBwIOXbjLUgQnJkjKXLP1Y0pdkcCSnvbOH-dane_ha72HTp44lMliE1jShZAeX8XxbR71TsswBfZTocNxMYLzrZzGruaXSGBzv9Fh4QXarFv-3dfY--pLB8qbAaCgYKAfgSARMSFQHGX2Mih0YIYk5IlaB1-1AUSvCD2w0293";
+  "<HIDDEN_FOR_PRIVACY>";
 
 export default function () {
   const payload = JSON.stringify({ token });
